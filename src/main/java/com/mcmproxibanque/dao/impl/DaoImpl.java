b4bb53bc9f24d7708887e4a2ae99c6ef6de08550
mathilde <mathilde.terrioux@gmail.com>
2016-12-16 15:33:35 +0100

    completed DaoImpl

--- src/main/java/com/mcmproxibanque/dao/impl/DaoImpl.java
+++ src/main/java/com/mcmproxibanque/dao/impl/DaoImpl.java
@@ -1,39 +1,89 @@
 package com.mcmproxibanque.dao.impl;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mcmproxibanque.dao.interfaces.IDao;
 
+import javassist.bytecode.SignatureAttribute.TypeVariable;
+
 public class DaoImpl<E> implements IDao<E> {
 
+	@PersistenceContext(unitName = "persistenceUnit")
+	protected EntityManager entityManager;
+	protected E instance;
+	private Class<E> entityClass;
+
+	@Transactional
 	@Override
 	public void persist(E e) throws Exception {
-		// TODO Auto-generated method stub
-		
+		getEntityManager().persist(e);
 	}
 
+	@Transactional
 	@Override
 	public void merge(E e) throws Exception {
-		// TODO Auto-generated method stub
-		
+		getEntityManager().merge(e);
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public void remove(Object id) throws Exception {
-		// TODO Auto-generated method stub
-		
+		getEntityManager().remove(findById(id));
 	}
 
+	@Transactional(readOnly=true)
 	@Override
 	public E findById(Object id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return (E) getEntityManager().find(getEntityClass(), id);
 	}
 
+	@Transactional(readOnly=true)
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<E> findAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return getEntityManager().createQuery("Select t from " + getEntityClass().getSimpleName() + "t")
+				.getResultList();
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Class<E> getEntityClass() throws Exception {
+		if (entityClass == null) {
+			Type type = getClass().getGenericSuperclass();
+			if (type instanceof ParameterizedType) {
+				ParameterizedType paramType = (ParameterizedType) type;
+				if (paramType.getActualTypeArguments().length == 2) {
+					if (paramType.getActualTypeArguments()[1] instanceof TypeVariable) {
+						throw new IllegalArgumentException("Can't find class using reflection");
+					} else {
+						entityClass = (Class<E>) paramType.getActualTypeArguments()[1];
+					}
+				} else {
+					entityClass = (Class<E>) paramType.getActualTypeArguments()[0];
+				}
+			} else {
+				throw new Exception("Can't find class using reflection");
+			}
+		}
+		return entityClass;
+	}
+
+	public void setEntityClass(Class<E> entityClass) {
+		this.entityClass = entityClass;
 	}
 
 }
