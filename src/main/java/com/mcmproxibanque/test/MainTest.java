6245490212f48d8c2d313e7f608f22d95335a4ce
Mathieu Langlais <mathieu.langlais@gmail.com>
2016-12-16 14:40:00 +0100

    Modif des models pour la persistence JPA2

--- src/main/java/com/mcmproxibanque/test/MainTest.java
+++ src/main/java/com/mcmproxibanque/test/MainTest.java
@@ -1,9 +1,12 @@
 package com.mcmproxibanque.test;
 
+import com.mcmproxibanque.model.Customer;
+
 public class MainTest {
 
 	public static void main(String[] args) {
-
+		Customer c = new Customer();
+		System.out.println(c);
 
 	}
 
